fe0f2b43bdbb39f1b8e53ebb6f8f1af3b9a045ef
Calvin Buckle <ccccbuckle@gmail.com>
2024-01-10 13:53:43 +0200

    setup hashMap class

---
+++ script.js
@@ -0,0 +1,22 @@
+class hashMap{
+
+
+
+  
+     stringToNumber(string) {
+            let hashCode = 0;
+          
+            const primeNumber = 31;
+            for (let i = 0; i < string.length; i++) {
+              hashCode = primeNumber * hashCode + string.charCodeAt(i);
+            }
+          
+            return hashCode;
+          }
+
+
+
+
+
+
+}

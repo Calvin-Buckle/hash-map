8773d9bbd0f00a7fd282f2de9c763dffebc34860
Calvin Buckle <ccccbuckle@gmail.com>
2024-01-11 13:21:30 +0200

    finish project

--- script.js
+++ script.js
@@ -18,6 +18,62 @@ class HashMap{
 table = new Array(16)
 
 
+set(value){
+let index = stringToNumber(value);
+this.table[index] = value;
+}
+
+get(key){
+let value = this.table[key];
+return value;
+}
+
+has(key){
+if(this.table[key])
+{return true}
+else{
+  return false;
+}
+}
+
+
+remove(key){
+  this.table.splice(key)
+
+}
+
+length(table){
+let count = 0
+this.table.forEach((key) => (count++))
+return count;
+}
+
+
+clear(){
+  while(this.table.length > 0) {
+    this.table.pop();
+}
+}
+
+
+
+keys(){
+  let array2 = []
+this.table.forEach((key) => array2.push(stringToNumber(key)));
+return array2;
+
+}
+
+
+
+
+values(){
+  let array2 = []
+  this.table.forEach((key) => array2.push(key));
+  return array2;
+
+}
+
 
 
 
